d9d41c0cbf1af52557493ffef641c1945a46ab8b
ajstudd <j7654894110@gmail.com>
2025-04-23 15:52:01 +0530

    added back the host

--- auth-service/src/server.ts
+++ auth-service/src/server.ts
@@ -3,6 +3,7 @@ import app from './app';
 import mongoose from 'mongoose';
 import mailer from './services/mail.service';
 
+const HOST = '0.0.0.0';
 mongoose
     .connect(process.env.MONGO_URL!)
     .then(() => {
@@ -11,7 +12,7 @@ mongoose
         mailer.init();
         console.log('Initialized Nodemailer!');
 
-        app.listen(process.env.PORT, () =>
+        app.listen(parseInt(process.env.PORT!, 10), HOST, () =>
             console.log('Started server on port', process.env.PORT)
         );
     })
